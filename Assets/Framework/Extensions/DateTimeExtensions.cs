65ffb4dd46473217c7e5ac8013cbc738a084d50a
Steve Harris <steve97harris@hotmail.co.uk>
2025-07-29 11:29:09 +0100

    fix datetime conversion

--- Assets/Framework/Extensions/DateTimeExtensions.cs
+++ Assets/Framework/Extensions/DateTimeExtensions.cs
@@ -3,10 +3,31 @@ namespace Framework.Extensions
 {
     public class DateTimeExtensions
     {
-        public static DateTime ConvertUtcTimeToGmt(DateTime utcDate)
+        public static DateTime ConvertUtcTimeToGmt(DateTime utcDateTime)
         {
-            return TimeZoneInfo.ConvertTimeFromUtc(utcDate,
-                TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+            try
+            {
+                TimeZoneInfo gmtZone;
+                
+                #if UNITY_IOS
+                    // iOS uses "GMT" as identifier
+                    gmtZone = TimeZoneInfo.FindSystemTimeZoneById("GMT");
+                #elif UNITY_ANDROID
+                    // Android uses "GMT" as identifier
+                    gmtZone = TimeZoneInfo.FindSystemTimeZoneById("GMT");
+                #else
+                    // Windows uses "GMT Standard Time"
+                    gmtZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+                #endif
+
+                return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, gmtZone);
+            }
+            catch (Exception ex)
+            {
+                // Fallback method if timezone conversion fails
+                UnityEngine.Debug.LogWarning($"Time zone conversion failed: {ex.Message}. Using UTC offset method instead.");
+                return utcDateTime.AddHours(0); // GMT is UTC+0
+            }
         }
     }
 }
